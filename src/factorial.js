8dc3e0aace37689d6008359c8e49801817f8db8c
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-03 07:35:04 +0530

    1 file modified

--- src/factorial.js
+++ src/factorial.js
@@ -1,14 +1,12 @@
 import React, { useMemo } from 'react';
 
-const Factorial = number => {
-  // const factorial = useMemo(() => calculateFactorialOf(number), [number])
-  // console.log("fact",factorial);
-  console.log({ number });
-  return <div />;
+const Factorial = props => {
+  const { number } = props;
+  const factorial = useMemo(() => calculateFactorialOf(number), [number]);
+  return <div>factorial is - {factorial}</div>;
 };
 
-// const calculateFactorialOf = number =>
-//   number <= 0 ? 1 : calculateFactorialOf(number - 1);
-
+const calculateFactorialOf = number =>
+  number <= 0 ? 1 : number * calculateFactorialOf(number - 1);
 
 export { Factorial };
